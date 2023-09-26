5d77adbc3d67b8203d9e38e067ef6a63c658e911
洪聖峻 <su3tldk394@gmail.com>
2023-09-26 09:57:13 +0800

    gogo

---
+++ app/Http/Controllers/EditorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seeall;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Validator;
+
+
+class EditorController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Frontend/See');
+    }
+
+    public function seeStore(Request $request)
+    {
+        $data = $request->json()->all();
+        // $data = json_decode($request->input('jsonData'), true);
+        // 定义验证规则
+        $rules = [
+            'title' => 'required|string',
+        ];
+
+        // 创建一个验证实例并应用规则
+        $validator = Validator::make($data, $rules);
+
+        // 检查验证是否通过
+        // if ($validator->fails()) {
+        //     // 验证失败，可以返回错误消息或采取其他措施
+        //     return response()->json(['errors' => $validator->errors()], 400);
+        // }
+        // dd($request->jsonData);
+        $see = Seeall::create([
+            'jsondata' => $request->jsonData,
+        ]);
+        return back()->with(['message' => rtFormat($see)]);
+   }
+}
--- routes/test.php
+++ routes/test.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\EditorController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return Inertia::render('Backend/Question');
 })->name('response');
+
+Route::get('/see',[EditorController::class,'index'])->name('see.index');
+
+Route::post('/see/store',[EditorController::class,'seeStore'])->name('see.store');
