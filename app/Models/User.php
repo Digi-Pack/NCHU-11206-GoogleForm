6c1c57327602b82389e20e600256f525e6a287de
tzuchi07 <140991232+tzuchi07@users.noreply.github.com>
2023-09-27 13:24:45 +0800

    users column connect to questions column

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function question ()
+    {
+        return $this->hasMany(Question::class,'lead_author_id','id');
+    }
 }
