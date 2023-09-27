521503008059a866dde1b84eaeff4f8f4ba831e0
洪聖峻 <su3tldk394@gmail.com>
2023-09-27 15:56:12 +0800

    可以拿取舊表單資料下一步要做編輯

---
+++ resources/js/Pages/Backend/Guideindex.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    response: {
+      type: Object,
+      required: true,
+      default: () => {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<template>
+  <section id="guideindex">
+    <div v-for="item in response.rt_data" :key="item.id">
+      <h1>{{ item.qu_naires_title }}</h1>
+      <Link :href="route('edit.old', { id: item.id })">
+        <button type="button" class="px-6 py-3 border border-black rounded-[5px]">編輯</button>
+      </Link>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#guideindex {}
+</style>
