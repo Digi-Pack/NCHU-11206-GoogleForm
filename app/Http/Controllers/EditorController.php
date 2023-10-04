1b24d31a0b2c67047031ad57f7976bb64775888f
tzuchi07 <140991232+tzuchi07@users.noreply.github.com>
2023-10-04 12:22:42 +0800

    修正 點選 (不歸我所有的表單)的報錯情況

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -68,8 +68,11 @@ class EditorController extends Controller
         // dd(session()->get('update_token'));
 
         // 先找到該用戶自己的表單，再找到指定id的表單，避免猜網址
-        $responseForm = Question::where('lead_author_id', $request->user()->id)->where('id', $request->id)->first();
+        $responseForm = Question::where('lead_author_id', $request->user()->id)->orWhereHas('coworker', function ($query) use ($request) {
+            return $query->where('coworker_id', $request->user()->id);
+        })->where('id', $request->id)->first();
         // dd($responseForm);
+
 
         if (!session()->get('update_token') == $request->id) {
             $responseForm->update([
