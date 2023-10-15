3027670933034cfc752915010dc8cc71b1da62f3
yuo906 <140991318+yuo906@users.noreply.github.com>
2023-10-15 09:42:21 +0800

    修正瀏覽器標題及logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>Google Form</title>
 
+        <link rel="icon" type="image/x-icon" href="./logo.png" rel="external nofollow" >
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
